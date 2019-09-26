b8ed4f6b59a27aecd450daa5a27adc660a2a36f3
daniele <krobi@cock.li>
2019-09-26 22:34:19 +0200

    Commit iniziale

---
+++ cmake-build-debug/CMakeFiles/Algorithm_Project_2019.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Algorithm_Project_2019"
+  "Algorithm_Project_2019.pdb"
+  "CMakeFiles/Algorithm_Project_2019.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Algorithm_Project_2019.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Algorithm_Project_2019.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Algorithm_Project_2019.
+# This may be replaced when dependencies are built.
