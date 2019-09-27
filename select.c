6949d6819af812269cd62778e9c0a48552bc3eaf
daniele <krobi@cock.li>
2019-09-27 17:22:44 +0200

    Aggiunta della funzione swap all'interno di select e correzione di bug

--- select.c
+++ select.c
@@ -2,4 +2,40 @@
 // Created by daniele on 9/27/19.
 //
 
+#include <values.h>
 #include "select.h"
+
+int partition (Array* a, int l, int r){
+    double x = a->array[r];
+    int i = l;
+    for (int j=l; j<r; j++){
+        if (a->array[j] <= x){
+            swap(&a->array[i], &a->array[j]);
+            i++;
+        }
+    }
+    swap(&a->array[i], &a->array[r]);
+    return i;
+}
+
+double kthSmallest(Array* a, int l, int r, int k){
+    if (k>0 && k<=(r-l+1)){
+
+        int index = partition(a, l, r);
+
+        if (index - l == k - 1){
+            return a->array[index];
+        }
+        if (index - l > k - 1){
+            return kthSmallest(a, l, index-1, k);
+        }
+        return kthSmallest(a, index+1, r, k-index+l-1);
+    }
+    return -1;
+}
+
+void swap (double* a, double *b){
+    double temp = *b;
+    *b = *a;
+    *a = temp;
+}
--- select.h
+++ select.h
@@ -2,7 +2,29 @@
 // Created by daniele on 9/27/19.
 //
 
+#include "arrayList.h"
+
 #ifndef ALGORITHM_PROJECT_2019_SELECT_H
 #define ALGORITHM_PROJECT_2019_SELECT_H
+/*
+ * Standard partition di QuickSort()
+ * Considera l'ultimo elemento come pivot
+ * e muove tutti gli elementi minori a sinistra di esso
+ * e quelli maggiori a destra
+ */
+int partition (Array* a, int l, int r);
+
+/*
+ * Questa funzione torna il k-esimo elemento piu' piccolo
+ * nell'array in posizione [l...r] usando un metodo basato su QuickSort
+ * ASSUNZIONE: Tutti gli elementi in a[] sono distinti
+ */
+
+double kthSmallest(Array* a, int l, int r, int k);
+
+/*
+ * Funzione di swap di due elementi
+ */
+void swap (double* a, double *b);
 
 #endif //ALGORITHM_PROJECT_2019_SELECT_H
