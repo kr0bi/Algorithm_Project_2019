918722475ddcd01ed16d56f2f863165d68413ffd
daniele <krobi@cock.li>
2019-09-27 17:20:16 +0200

    implementazione del (forse) algoritmo ottimale per il calcolo della media pesata

--- weighted_median.c
+++ weighted_median.c
@@ -3,3 +3,36 @@
 //
 
 #include "weighted_median.h"
+#include "select.h"
+#include "sum.h"
+
+double weighted_median_sort(Array *ptr){
+
+}
+
+double weighted_median_optimal(Array *ptr, int p, int r, double w){
+    if (r==p){
+        return ptr->array[p];
+    }
+    if (r-p == 1){
+        if (ptr->array[p] >= ptr->array[r]){
+            return ptr->array[p];
+        } else {
+            return ptr->array[r];
+        }
+    }
+    int q = partition(ptr, p, r);
+    double wl = sum_partitions(ptr, p, q-1);
+    double wg = sum_partitions(ptr, q+1, r);
+
+    if (wl < w/2 && wg < w/2 ){
+        return ptr->array[q];
+    } else {
+        if (wl > wg){
+            weighted_median_optimal(ptr, p, q, w-wg);
+        } else {
+            weighted_median_optimal(ptr, q, r, w+wl);
+        }
+    }
+
+}
--- weighted_median.h
+++ weighted_median.h
@@ -5,4 +5,10 @@
 #ifndef ALGORITHM_PROJECT_2019_WEIGHTED_MEDIAN_H
 #define ALGORITHM_PROJECT_2019_WEIGHTED_MEDIAN_H
 
+#include "arrayList.h"
+
+double weighted_median_sort(Array *ptr);
+
+double weighted_median_optimal(Array *ptr, int p, int r, double w);
+
 #endif //ALGORITHM_PROJECT_2019_WEIGHTED_MEDIAN_H
