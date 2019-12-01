f80e7954ba4f21f6de6dcf88026da0da3b0139ce
krobi <krobi@cock.li>
2019-12-01 18:58:17 +0100

    Aggiunta la fase di inserimento dell'input

--- main.c
+++ main.c
@@ -5,23 +5,31 @@
 
 int main(int argc, char **argv) {
     //controllo input
-    if (argc == 0){
+    /*if (argc == 0){
 
-    }
+    }*/
 
     //inserimento input
     int size = 1;
     double *array = malloc(size*sizeof(double));
     int read = 0;
-    while (scanf("%lf%*[,]", &array[read]) == 1) {
+    while (scanf("%lf%*[ ,]", &array[read]) >= 1) {
+    //while (scanf("%lf%*[,]", &array[read]) >= 1) {
         if(++read == size) {
-            size += 1;
+            size *= 2;
             array = realloc(array, size*sizeof(double));
         }
     }
+
+    for (int i =0; i<read; i++){
+        printf("%lf ", array[i]);
+    }
+    printf("\n");
+
     //inizio algoritmo
     //double array[] = {0.1 , 0.35 , 0.05, 0.1, 0.15 , 0.05, 0.2 };
     //int size2 = (int)(sizeof(array)/sizeof(array[0]));
-    start_algorithm(array, size);
+    start_algorithm(array, read);
+
     return 0;
 }
