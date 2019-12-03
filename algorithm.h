9822b99ff56ed94cbf134b3c0f29c538508af44e
krobi <krobi@cock.li>
2019-12-03 23:10:29 +0100

    aggiunto algoritmo print per verificare l'output per il progetto

--- algorithm.h
+++ algorithm.h
@@ -4,6 +4,7 @@
 
 #ifndef ALGORITHM_PROJECT_2019_ALGORITHM_H
 #define ALGORITHM_PROJECT_2019_ALGORITHM_H
+void start_algorithm_print(double *array, int size);
 void start_algorithm(double *array, int size);
 
 #endif //ALGORITHM_PROJECT_2019_ALGORITHM_H
