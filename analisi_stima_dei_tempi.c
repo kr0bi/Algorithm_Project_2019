ef68cfcb60add4d7a4971999e13daab00dd6fd5c
krobi <krobi@cock.li>
2019-12-11 16:15:48 +0100

    aggiunta dell'array di puntatori alle funzioni

--- analisi_stima_dei_tempi.c
+++ analisi_stima_dei_tempi.c
@@ -7,22 +7,6 @@
 #include "algorithm.h"
 #include <time.h>
 
-/*long granularita2 (){
-    struct timespec t0;
-    clock_gettime(CLOCK_MONOTONIC, &t0);
-    struct timespec t1;
-    clock_gettime(CLOCK_MONOTONIC, &t1);
-    while (t1.tv_nsec == t0.tv_nsec && t1.tv_sec == t0.tv_sec){
-        clock_gettime(CLOCK_MONOTONIC, &t1);
-    }
-    long res = (t1.tv_sec-t0.tv_sec)*1000000000+(t1.tv_nsec-t0.tv_nsec);
-    return res;
-}
-
-double get_t_min2 (long granularita, double tolleranza){
-    return (double)(granularita/tolleranza);
-}*/
-
 double granularita (){
     clock_t t0 = clock();
     clock_t t1 = clock();
@@ -34,14 +18,16 @@ double granularita (){
 double get_t_min(double granularita, double tolleranza){
     return (double)(granularita/tolleranza);
 }
-double calcola_rip_version_function(void (*start)(double*, int), double *array, int size, double t_min){
+double calcola_rip(void (**handler_of_functions)(double*, int), int number_of_methods, double *array, int size, double t_min){
     clock_t t0, t1;
     int rip = 1;
     while ((double)(t1-t0)<=t_min){
         rip *= 2;
         t0 = clock();
         for (int i = 1; i<rip; i++){
-            (*start)(array, size);
+            for (int j=0; j<number_of_methods; j++) {
+                (*handler_of_functions[j])(array, size);
+            }
         }
         t1 = clock();
     }
@@ -52,7 +38,9 @@ double calcola_rip_version_function(void (*start)(double*, int), double *array, 
         rip = (max+min)/2;
         t0 = clock();
         for (int i =1 ; i<rip; i++){
-            (*start)(array, size);
+            for (int j=0; i<number_of_methods; j++) {
+                (*handler_of_functions[j])(array, size);
+            }
         }
         t1 = clock();
         if ((double)(t1-t0)<=t_min){
@@ -65,8 +53,10 @@ double calcola_rip_version_function(void (*start)(double*, int), double *array, 
     return max;
 }
 
+void prepara(double* array, int size){
 
-double calcola_rip(double *array, int size, double t_min){
+}
+/*double calcola_rip(double *array, int size, double t_min){
     clock_t t0, t1;
     int rip = 1;
     while ((double)(t1-t0)<=t_min){
@@ -95,7 +85,7 @@ double calcola_rip(double *array, int size, double t_min){
         }
     }
     return max;
-}
+}*/
 
 double calcola_rip_tara(double *array, int size, double t_min){
     clock_t t0, t1;
@@ -138,3 +128,20 @@ void calcolo_dei_tempi(double *array, int size, double rip){
     double t_sing = t_tot/rip; //tempo medio di esecuzione
     printf("t_tot: %lf\nt_medio: %lf", t_tot, t_sing);
 }
+
+
+/*long granularita2 (){
+    struct timespec t0;
+    clock_gettime(CLOCK_MONOTONIC, &t0);
+    struct timespec t1;
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    while (t1.tv_nsec == t0.tv_nsec && t1.tv_sec == t0.tv_sec){
+        clock_gettime(CLOCK_MONOTONIC, &t1);
+    }
+    long res = (t1.tv_sec-t0.tv_sec)*1000000000+(t1.tv_nsec-t0.tv_nsec);
+    return res;
+}
+
+double get_t_min2 (long granularita, double tolleranza){
+    return (double)(granularita/tolleranza);
+}*/
--- analisi_stima_dei_tempi.h
+++ analisi_stima_dei_tempi.h
@@ -8,8 +8,11 @@
 //double get_t_min2 (long granularita, double tolleranza);
 double granularita();
 double get_t_min(double granularita, double tolleranza);
-double calcola_rip_version_function(void (*start)(double*, int), double *array, int size, double t_min);
-double calcola_rip(double *array, int size, double t_min);
+
+double calcola_rip(void (**handler_of_methods)(double*, int), int number_of_methods, double *array, int size, double t_min);
+void prepara (double* array, int size);
+/*double calcola_rip(void (*start)(double*, int), double *array, int size, double t_min);*/
+/*double calcola_rip(double *array, int size, double t_min);*/
 double calcola_rip_tara(double *array, int size, double t_min);
 void calcolo_dei_tempi(double *array, int size, double rip);
 #endif //ALGORITHM_PROJECT_2019_ANALISI_STIMA_DEI_TEMPI_H
