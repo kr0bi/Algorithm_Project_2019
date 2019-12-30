062df91e46ceba9b06e32dcd99484dbd191f0a8c
krobi <krobi@cock.li>
2019-12-30 12:47:13 +0100

    aggiunta dei commenti

--- weighted_median.c
+++ weighted_median.c
@@ -6,6 +6,12 @@
 #include "sum.h"
 #include "sort.h"
 
+/**
+ * algoritmo di risoluzione del problema della mediana inferiore pesata
+ * @param ptr array di cui trovare la mediana inferiore pesata
+ * @param size dimensione dell'array
+ * @return la mediana inferiore pesata
+ */
 double weighted_median_sort(double *ptr, int size){
     double W = sum_partitions(ptr, 0, size-1);
     sort(ptr, size);
