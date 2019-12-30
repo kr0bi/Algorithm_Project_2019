3f9e9d37bb6b486a965c558427577ae2dfcb7589
krobi <krobi@cock.li>
2019-12-30 12:46:11 +0100

    aggiunta dei commenti

--- algorithm.c
+++ algorithm.c
@@ -4,11 +4,21 @@
 #include <stdio.h>
 #include "weighted_median.h"
 
+/**
+ * fa partire l'algoritmo risolutivo e stampa il risultato
+ * @param array
+ * @param size
+ */
 void start_algorithm_print(double* array, int size){
     double med_sort = weighted_median_sort(array, size);
     printf("%.15f\n", med_sort);
 }
 
+/**
+ * fa partire l'algoritmo risolutivo senza stamparne il risultato
+ * @param array
+ * @param size
+ */
 void start_algorithm(double* array, int size){
     double med_sort = weighted_median_sort(array, size);
     //printf("%.8f\n", med_sort);
