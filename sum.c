398a34bb8b6272430e553683df1e54e89c38a96b
krobi <krobi@cock.li>
2019-12-30 12:46:58 +0100

    aggiunta dei commenti

--- sum.c
+++ sum.c
@@ -2,6 +2,13 @@
 // Created by daniele on 9/27/19.
 //
 #include "sum.h"
+/**
+ * somma i valori tra l'indice p e q compresi
+ * @param ptr array da passare
+ * @param p indice iniziale
+ * @param q indice finale
+ * @return
+ */
 double sum_partitions(double *ptr, int p, int q){
     double sum = 0.0;
     for (int i = p; i<=q; i++){
--- sum.h
+++ sum.h
@@ -3,6 +3,5 @@
 //
 #ifndef ALGORITHM_PROJECT_2019_SUM_H
 #define ALGORITHM_PROJECT_2019_SUM_H
-//Somma i valori dall'indice p all'indice q dell'array
 double sum_partitions(double *ptr, int p, int q);
 #endif //ALGORITHM_PROJECT_2019_SUM_H
